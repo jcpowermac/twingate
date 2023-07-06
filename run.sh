13fc2ed0148526f8de0339376412c09deda5b05c
Joseph Callen <jcallen@redhat.com>
2023-07-06 14:22:24 -0400

    use hostname to select service account

--- run.sh
+++ run.sh
@@ -4,16 +4,19 @@ source network-check.sh
 
 trap "cd /tmp; twingate report; cat /var/log/twingated.log; twingate stop" EXIT;
 
+
+filenumber=$(shuf -n 1 -i 1-4 --random-source <(printf "%s" "$HOSTNAME"))
+
 sleep 5
 twingate --version
-twingate setup --headless /secret/credentials.json
+twingate setup --headless "/secret/credentials${filenumber}.json"
 twingate config log-level debug
 twingate start
 
 while true; do
     sleep 60;
     check_range
-    if [ $RESULT == 1 ]
+    if [ "$RESULT" == 1 ]
     then
         exit 1
     fi
