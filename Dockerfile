0bcdf54c9c6d1a972a232f4c5e72abf7ec5cc869
Joseph Callen <jcallen@redhat.com>
2023-07-12 16:48:52 -0400

    use powershell and powercli for testing

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
-FROM quay.io/app-sre/ubi8-ubi:latest
+#FROM quay.io/app-sre/ubi8-ubi:latest
+FROM mcr.microsoft.com/powershell:ubi-9
 
-COPY run.sh network-check.sh /
+COPY run.ps1 run.sh network-check.sh /
+
+RUN curl -L -o - "https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz" | tar -C /bin -xvzf - govc
+
+RUN export POWERSHELL_TELEMETRY_OPTOUT=1 && \
+	pwsh -NoLogo -NoProfile -Command " \
+          \$ErrorActionPreference = 'Stop' ; \
+          \$ProgressPreference = 'SilentlyContinue' ; \
+          Set-PSRepository -Name PSGallery -InstallationPolicy Trusted ; \
+          Install-Module -Force -Scope AllUsers PSSlack ; \
+          Install-Module -Scope AllUsers VMware.PowerCLI ; \
+          Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCeip:\$false -Confirm:\$false"
 
 RUN dnf upgrade --refresh -y && \
     curl https://binaries.twingate.com/client/linux/install.sh | bash && \
     dnf install iproute iputils -y
 
-#ENTRYPOINT [ "twingate", "start" ]
-#CMD ["tail", "-f", "/dev/null"]
-
-ENTRYPOINT ["/run.sh"]
+ENTRYPOINT ["/run.ps1"]
---
+++ run.ps1
@@ -0,0 +1,50 @@
+#!/bin/pwsh
+
+. /var/run/config/vcenter/variables.ps1
+Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out-Null
+
+$cihash = ConvertFrom-Json -InputObject $ci -AsHashtable
+
+$errmessage = @"
+vCenter: {0}
+Error: {1}
+Hostname: $($Env:HOSTNAME)
+Twingate restarted
+"@
+
+$fileNumber = Get-Random -Minimum 1 -Maximum 16
+
+twingate --version
+twingate setup --headless "/secret/credentials$($fileNumber).json"
+twingate config log-level debug
+twingate start
+
+# run forever
+while ($true) {
+    Start-Sleep -Seconds 60
+    # Only check ibmc
+    $key = "ibm8"
+
+    try {
+        Connect-VIServer -Server $cihash[$key].vcenter -Credential (Import-Clixml $cihash[$key].secret) | Out-Null
+        $vm = Get-VM
+        $vm.Count
+    }
+    catch {
+        $caught = Get-Error
+        $errStr = $caught.ToString()
+
+        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($errmessage -f $cihash[$key].vcenter, $errStr)
+
+        twingate report
+        twingate stop
+        Start-Sleep -Seconds 10
+        twingate start
+
+    }
+    finally {
+        Disconnect-VIServer -Server * -Force:$true -Confirm:$false
+    }
+}
+
+exit 0
